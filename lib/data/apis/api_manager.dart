3efe123448b2c9f41b0c811042b0a20f3b80af88
Mohamed <mohamed.osama200538@gmail.com>
2024-04-17 18:00:44 +0200

    Created ApiManager and made getWeather fun to return weather api response

--- lib/data/apis/api_manager.dart
+++ lib/data/apis/api_manager.dart
@@ -1,1 +1,21 @@
-class ApiManager {}
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:weather_app/data/models/weather_model.dart';
+
+class ApiManager {
+  static const String baseUrl = "api.weatherapi.com";
+  static const String apiKey = "8f25f70db2b7455095a65059241604";
+
+  Future<WeatherModel> getWeather(String city) async {
+    Uri url = Uri.https(baseUrl, "v1/forecast.json", {
+      "key": apiKey,
+      "q": city,
+    });
+
+    http.Response response = await http.get(url);
+    var json = jsonDecode(response.body);
+    var weatherResponce = WeatherModel.fromJson(json);
+    return weatherResponce;
+  }
+}
